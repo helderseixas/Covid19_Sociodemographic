4adcce887fc93e69c2c984d0c46d3a956da21abc
helderseixas <helderseixas@gmail.com>
2024-07-11 23:14:03 -0300

    General adjustments.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Sociodemographic Factors Associated with Covid-19 Mortality in Brazilian Municipalities Across Three Years: An Approach Supported by Clustering
+# Sociodemographic Factors Associated with Covid-19 Mortality in Brazilian Municipalities Across Three Years: an Approach Supported by Gaussian Mixture Clustering
 
-Welcome to the repository for the code and data of the paper "Sociodemographic Factors Associated with Covid-19 Mortality in Brazilian Municipalities Across Three Years: An Approach Supported by Clustering."
+Welcome to the repository for the code and data of the paper "Sociodemographic Factors Associated with Covid-19 Mortality in Brazilian Municipalities Across Three Years: an Approach Supported by Gaussian Mixture Clustering."
 
 **Citation:**
 If you utilize this code and data for your research, please cite our work as follows:
@@ -11,35 +11,15 @@ Hélder Seixas Lima, Petrônio Cândido de Lima e Silva, Wagner Meira Jr. et al.
 
 To reproduce the results presented in our paper, please follow the steps outlined below:
 
-1. **Download and Prepare Datasets:**
+1. **Prepare Datasets:**
 
    - **Covid-19 Data:**
-     - Download Covid-19 data from the Sistema de Informação sobre Mortalidade (https://opendatasus.saude.gov.br/fa_IR/dataset/sim) for the years 2020, 2021, and 2022.
-     - Place the downloaded files in the directory `covid/data/input`.
+     - Download Covid-19 data (https://mega.nz/file/M8ECERAa#zl_OdBVAaIfFUcNR7LiY13OCoI2Zsi-Gq-tU0e1pmxA).
+     - Extract and place the downloaded files in the directory `data/input`.
 
-   - **Sociodemographic Data:**
-     - Extract the file `covid/data/df_municipal.7z`.
-     - This file contains the municipality database with various sociodemographic attributes.
 
-   - **Mortality Level Data:**
-     - We have provided the mortality level data used in our paper in the file `mortality_levels/data/data.7z`.
-     - You can choose to analyze the same data we used or reproduce this data by executing all notebooks in the `mortality_levels` folder.
+2. **Generate Results:**
 
-   - **Correlation Data:**
-     - Similarly, the correlation data we used in our analysis is provided in the file `correlation_sociodemographic_covid/data/data.7z`.
-     - You can decide whether to analyze the provided data or regenerate it by executing the notebooks in the `correlation_sociodemographic_covid` folder.
-
-2. **Paper Figures:**
-
-   - You can directly obtain the figures from our paper without running the scripts by extracting the following files:
-     - `covid/images/images.7z`
-     - `correlation_sociodemographic_covid/images/images.7z`
-
-3. **Generate Results:**
-
-   - Execute the notebooks in the following folders to generate results:
-     - `covid`
-     - `mortality_levels`
-     - `correlation_sociodemographic_covid`
+   - Execute the notebooks to reproduce the results.
 
 Please feel free to reach out if you have any questions or need assistance. Happy exploring and reproducing the findings of our study!
--- util.py
+++ util.py
@@ -1,41 +1,88 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import gamma
-import epyestim
-import epyestim.covid19 as covid19
-plt.style.use('seaborn-white')
-from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+import statsmodels.api as sm
+import pickle
+import scipy.stats as stats
 import seaborn as sns
+
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 colors_default = {'extremamente_maior': '#80191C', 'muito_maior': '#D7191C', 'maior': '#FDAE61', 'medio': '#F09CFA',
                   'menor': '#ABD9E9', 'muito_menor': '#2c7bb6'}
 
-def get_default_colors_maps(n):
-    colors_custom = [colors_default['muito_menor'], colors_default['menor'], colors_default['medio'],
-                     colors_default['maior'], colors_default['muito_maior'], colors_default['extremamente_maior']]
-    pallet_custom = ListedColormap(colors_custom[:n], name='map')
-    return pallet_custom
+def tunning_negative_binomial_model(x, y, list_offset):
+    list_nb_glm_models = np.array([])
+    list_llf_models = np.array([])
+    list_alpha = np.arange(0.01, 2.1, 0.01)
 
-def get_default_colors_divergence_seaborn(n, reverse=False):
-    colors_custom = [colors_default['muito_menor'], colors_default['menor'], colors_default['medio'],
-                     colors_default['maior'], colors_default['muito_maior'], colors_default['extremamente_maior']]
+    for alpha in list_alpha:
+        nb_glm_model = sm.GLM(y, x, family=sm.families.NegativeBinomial(alpha=alpha), offset=list_offset).fit()
+        llf = nb_glm_model.llf
+        list_nb_glm_models = np.append(list_nb_glm_models, nb_glm_model)
+        list_llf_models = np.append(list_llf_models, llf)
 
-    if reverse == True:
-        pallet_custom = sns.color_palette(list(reversed(colors_custom[:n])))
-    else:
-        pallet_custom = sns.color_palette(colors_custom[:n])
-    return pallet_custom
+    # print('Selected alpha:', list_alpha[abs(list_llf_models) == min(abs(list_llf_models))][0])
+    # return list_nb_glm_models[abs(list_llf_models) == min(abs(list_llf_models))][0]
+    print('Selected alpha:', list_alpha[list_llf_models == max(list_llf_models)][0])
+    return list_nb_glm_models[list_llf_models == max(list_llf_models)][0]
 
-def get_default_colors_categorical_seaborn(n=5):
-    colors_custom = ['#FEC4DC', '#3F80B3', '#610099', '#CCA86C', '#91E693']
-    pallet_custom = sns.color_palette(colors_custom[:n])
-    return pallet_custom
+def save_model(model, name, directory_base='models'):
+    with open(directory_base+'/'+name+'.pkl', 'wb') as file:
+        pickle.dump(model, file)
 
-def get_default_colors_temporal_series_highlighting_peaks_seaborn(n=5):
-    colors_custom = ['#C7C7C7', '#FF2B2B', '#3331E6', '#FFB60F', '#BF00F5']
-    pallet_custom = sns.color_palette(colors_custom[:n])
-    return pallet_custom
+def summarize_results(model):
+    print(model.summary())
+    print('llf:', round(model.llf, 2))
+    try:
+        print('deviance: ', round(model.deviance, 2))
+        print('pearson_chi2: ', round(model.pearson_chi2, 2))
+        print("Estimated dispersion: ", round(model.pearson_chi2/model.df_resid,2))
+    except AttributeError:
+        pass
+    print('aic: ', round(model.aic, 2))
+
+
+def outlier_analysis(model):
+    influence = model.get_influence()
+    standardized_residuals = model.resid_pearson
+
+    cooks_d = influence.cooks_distance[0]
+
+    plt.figure(figsize=(12, 6))
+
+    # Plot standardized residuals
+    plt.subplot(1, 2, 1)
+    plt.scatter(range(len(standardized_residuals)), standardized_residuals)
+    plt.axhline(y=3, color='r', linestyle='--')
+    plt.axhline(y=-3, color='r', linestyle='--')
+    plt.title('Standardized Residuals')
+    plt.xlabel('Observation')
+    plt.ylabel('Standardized Residual')
+
+    # Plot Cook's distance
+    plt.subplot(1, 2, 2)
+    plt.scatter(range(len(cooks_d)), cooks_d)
+    plt.axhline(y=4 / model.nobs, color='r', linestyle='--')
+    plt.title("Cook's Distance")
+    plt.xlabel('Observation')
+    plt.ylabel("Cook's Distance")
+
+    plt.tight_layout()
+    plt.show()
+
+    outliers = np.where(np.abs(standardized_residuals) > 3)[0]
+    influential_points = np.where(cooks_d > 4 / model.nobs)[0]
+
+    print("Quantity outliers (Standardized Residuals > 3):", len(outliers))
+    print("Quantity influential Points (Cook's Distance > 4/n):", len(influential_points))
+
+    return outliers, influential_points, cooks_d, standardized_residuals
+
+def calculate_95_ci(data):
+    mean = data.mean()
+    sem = stats.sem(data)
+    margin_of_error = sem * stats.t.ppf((1 + 0.95) / 2., len(data) - 1)
+    return mean, mean - margin_of_error, mean + margin_of_error
 
 def get_default_colors_heatmap():
     colors = [[0, 'darkblue'],
@@ -47,3 +94,14 @@ def get_default_colors_heatmap():
 def centimeter_to_inch(centimeters):
     return centimeters * 1/2.54
 
+def get_default_colors_seaborn(n):
+    colors_custom = [colors_default['muito_menor'], colors_default['menor'], colors_default['medio'],
+                     colors_default['maior'], colors_default['muito_maior'], colors_default['extremamente_maior']]
+    pallet_custom = sns.color_palette(colors_custom[:n])
+    return pallet_custom
+
+def get_default_colors_maps(n):
+    colors_custom = [colors_default['muito_menor'], colors_default['menor'], colors_default['medio'],
+                     colors_default['maior'], colors_default['muito_maior'], colors_default['extremamente_maior']]
+    pallet_custom = ListedColormap(colors_custom[:n], name='map')
+    return pallet_custom
